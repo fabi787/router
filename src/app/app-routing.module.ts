c73e0e06beffa33d177d99c73661ebbe93614a04
fabi787 <fo4730350@gmail.com>
2021-11-23 19:09:22 -0300

    atividade das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './paste1/home/home.component';
+import { SobreComponent } from './paste2/sobre/sobre.component';
+import { ContatoComponent } from './paste3/contato/contato.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent},
+    {
+      path:'sobre',
+      component:SobreComponent
+    },
+    {
+      path:'contato',
+      component:ContatoComponent
+    }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './paste1/home/home.component';
+import { SobreComponent } from './paste2/sobre/sobre.component';
+import { ContatoComponent } from './paste3/contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
